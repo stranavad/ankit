def01f3ae3cadf8a5fcd1aff6b266ac47a9faaf7
stranavadavid <stranavadavid@protonmail.com>
2022-10-19 20:11:02 +0200

    changed few things

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
-/** @type {import('next').NextConfig} */
+/* eslint-disable no-undef */
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require("path");
+/** @type {import("next").NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+    reactStrictMode: true,
+    swcMinify: true,
+    sassOptions: {
+        includePaths: [path.join(__dirname, "styles")],
+    },
+    images: {
+        domains: ["lh3.googleusercontent.com"],
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+// import "@/styles/index.scss";
+import type {AppProps} from "next/app";
+import {SessionProvider} from "next-auth/react";
+import SecurePage from "@/pages/_secure";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+// @ts-ignore
+function MyApp({Component, pageProps: {session, ...pageProps}}: AppProps) {
+    return (
+        <SessionProvider session={session}>
+            <div className="main-wrapper">
+                <SecurePage><Component {...pageProps} /></SecurePage>
+            </div>
+        </SessionProvider>
+    );
 }
 
-export default MyApp
+export default MyApp;
