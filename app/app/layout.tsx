30a0e08bb7ac58f3914965c4baf87b6b8801dbf0
stranavadavid <stranavadavid@protonmail.com>
2023-02-04 22:27:51 +0100

    Fixed overlays on /app

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -7,17 +7,18 @@ import {Bars3Icon, ChevronDoubleRightIcon} from "@heroicons/react/24/outline";
 import Link from "next/link";
 import Sidebar from "@/components/Sidebar";
 
-export default function AppLayout(props: any) {
+const AppLayout = (props: any) => {
     const [topBarData, setTopBarData] = useState<Omit<TopBarContextData, "setSpace" | "setQuestionnaire">>({
         space: null,
         questionnaire: null
     });
 
+    const pathname = usePathname();
+
     const setSpace = (space: TopBarItem) => {
         setTopBarData(data => ({...data, space}));
     };
 
-    const pathname = usePathname();
 
     const setQuestionnaire = (questionnaire: TopBarItem) => {
         setTopBarData(data => ({...data, questionnaire}));
@@ -41,32 +42,40 @@ export default function AppLayout(props: any) {
         }
     }, [pathname]);
 
+    const showOverlays = (pathname?.split("/").length || 0) > 2;
+
     return (
         <TopBarContext.Provider value={{...topBarData, setSpace, setQuestionnaire}}>
             <SessionProvider session={props.session}>
-                <Sidebar/>
-                <nav
-                    className="bg-slate-900 fixed left-0 sm:left-14 top-0 right-0 h-14 py-1 px-4 flex items-center justify-start">
-                    <Bars3Icon className="sm:hidden w-6 h-6 text-white"/>
-                    {topBarData.space && (
-                        <>
-                            <Link href={topBarData.space.path}
-                                  className="text-slate-100 text-xl font-medium">{topBarData.space.title}</Link>
-
-                            {topBarData.questionnaire && (
+                {showOverlays && (
+                    <>
+                        <Sidebar/>
+                        <nav
+                            className="bg-slate-900 fixed left-0 sm:left-14 top-0 right-0 h-14 py-1 px-4 flex items-center justify-start">
+                            <Bars3Icon className="sm:hidden w-6 h-6 text-white"/>
+                            {topBarData.space && (
                                 <>
-                                    <ChevronDoubleRightIcon className="h-4 w-4 text-white mx-2"/>
-                                    <Link href={topBarData.questionnaire.path}
-                                          className="text-slate-100 text-lg font-medium">{topBarData.questionnaire.title}</Link>
+                                    <Link href={topBarData.space.path}
+                                          className="text-slate-100 text-xl font-medium">{topBarData.space.title}</Link>
+
+                                    {topBarData.questionnaire && (
+                                        <>
+                                            <ChevronDoubleRightIcon className="h-4 w-4 text-white mx-2"/>
+                                            <Link href={topBarData.questionnaire.path}
+                                                  className="text-slate-100 text-lg font-medium">{topBarData.questionnaire.title}</Link>
+                                        </>
+                                    )}
                                 </>
                             )}
-                        </>
-                    )}
-                </nav>
-                <div className="mt-20 ml-0 sm:ml-[3.5rem]">
+                        </nav>
+                    </>
+                )}
+                <div className={showOverlays ? "mt-20 ml-0 sm:ml-[3.5rem]" : ""}>
                     {props.children}
                 </div>
             </SessionProvider>
         </TopBarContext.Provider>
     );
-}
+};
+
+export default AppLayout;
